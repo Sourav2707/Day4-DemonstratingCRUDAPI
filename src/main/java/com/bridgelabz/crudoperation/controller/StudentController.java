fa7453d25264b2328f876f1d9c9ecbadc2dccf26
Sourav2707 <smartsourav33@gmail.com>
2023-06-19 16:09:50 +0530

    UC2 Added put method to the app

--- src/main/java/com/bridgelabz/crudoperation/controller/StudentController.java
+++ src/main/java/com/bridgelabz/crudoperation/controller/StudentController.java
@@ -4,10 +4,7 @@ import com.bridgelabz.crudoperation.entity.StudentEntity;
 import com.bridgelabz.crudoperation.repository.StudentRepository;
 import com.bridgelabz.crudoperation.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -20,6 +17,15 @@ public class StudentController {
      */
     @RequestMapping(method = RequestMethod.POST, value="/subjects")
     public void addSubject(@RequestBody StudentEntity subject)
+    {
+        studentService.addSubject(subject);
+    }
+    /**
+     * Demonstrating post method using @RequestMapping
+     * @param subject
+     */
+    @RequestMapping(method = RequestMethod.PUT, value="/subjects")
+    public void updateSubject( @RequestBody StudentEntity subject)
     {
         studentService.addSubject(subject);
     }
